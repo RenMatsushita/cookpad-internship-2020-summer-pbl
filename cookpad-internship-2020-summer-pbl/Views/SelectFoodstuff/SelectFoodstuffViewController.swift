9b537b098a80cb184d9136f771a1dd901d769ad9
RenMatsushita <ren.git.x@gmail.com>
2020-08-26 18:19:53 +0900

    initial commit

---
+++ cookpad-internship-2020-summer-pbl/Views/SelectFoodstuff/SelectFoodstuffViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  cookpad-internship-2020-summer-pbl
+//
+//  Created by Ren Matsushita on 2020/08/26.
+//  Copyright © 2020 Ren Matsushita. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+final class SelectFoodstuffViewController: UIViewController {
+    private lazy var viewModel: SelectFoodstuffViewModel = .init()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ cookpad-internship-2020-summer-pbl/Views/SelectFoodstuff/SelectFoodstuffViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SelectFoodstuffViewModel.swift
+//  cookpad-internship-2020-summer-pbl
+//
+//  Created by Ren Matsushita on 2020/08/26.
+//  Copyright © 2020 Ren Matsushita. All rights reserved.
+//
+
+import Foundation
+
+final class SelectFoodstuffViewModel {
+    init() {
+        
+    }
+}
